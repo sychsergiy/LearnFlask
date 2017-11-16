11bef1fc018351af83983c15bce8c72ff67923ac
Sych Sergiy <sychsergiy@gmail.com>
2017-11-16 16:02:18 +0200

    bootstrap base templates added

--- learning_flask.py
+++ learning_flask.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
