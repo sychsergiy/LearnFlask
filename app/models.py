d8aee782e008f0d7e192e6648a364ff4eb7fcb43
Sych Sergiy <sychsergiy@gmail.com>
2017-11-18 12:05:57 +0200

    password field and migration file added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from . import db
 
 
@@ -5,6 +7,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+
     users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
@@ -15,7 +18,20 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+    @property
+    def password(self):
+        raise AttributeError('password is not readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
