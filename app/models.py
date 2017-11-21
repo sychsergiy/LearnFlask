e2f7cf74f440ffccf9c967137a913ce4866c4051
Sych Sergiy <sychsergiy@gmail.com>
2017-11-21 09:38:08 +0200

    User avatar

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
@@ -165,6 +167,12 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         db.session.commit()
         return True
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        url = 'https://secure.gravatar.com/avatar'
+        hash = hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating)
 
     def can(self, perm):
         return self.role is not None and self.role.has_permission(perm)
